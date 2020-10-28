5c7c098fc8a8ae7c7dfbfa840901e6e39f61e3c3
Jordan Turner <jturne19@uwyo.edu>
2020-10-28 14:42:40 -0600

    add new file combining cluster_cat and gmc_cat

---
+++ workflow/catalog_setup.py
@@ -0,0 +1,32 @@
+"""
+main script for compiling the GMC and cluster catalog info and creating all the ncessary ds9 region files
+
+2020-10-27
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+from astropy.io import fits, ascii
+import sys
+
+sys.path.append('/cherokee1/turner/phangs/cf/utils')
+from gmc_cat_utils import *
+from cluster_cat_utils import *
+
+# set path of the data dir
+data_dir = '/cherokee1/turner/phangs/cf/data'
+
+# read in list of galaxies 
+master_galaxy_list = ascii.read('master_galaxy.list')
+galaxy_list = ascii.read('galaxy.list')
+
+# create ds9 region files for all the galaxies
+all_galaxies_gmc_region_files(master_galaxy_list, data_dir)
+
+# print out number of clusters and make the new fits files if needed
+all_galaxies_clust_cats(master_galaxy_list, data_dir, mkfits=False)
+
+# create ds9 region fiels for all the galaxies
+all_galaxies_clust_region_files(master_galaxy_list, data_dir, radius_pix=10)
+
+
