ed1c6977ce92c7800175d3baa43ef544a85164d9
Jordan Turner <jturne19@uwyo.edu>
2020-10-28 15:58:19 -0600

    add call to the function to make the outline plots

--- workflow/catalog_setup.py
+++ workflow/catalog_setup.py
@@ -12,22 +12,26 @@ import sys
 sys.path.append('/cherokee1/turner/phangs/cf/utils')
 from gmc_cat_utils import *
 from cluster_cat_utils import *
+from image_utils import *
+from plot_utils import *
 
 # set path of the data dir
-data_dir = '/cherokee1/turner/phangs/cf/data'
+data_dir = '/cherokee1/turner/phangs/cf/data/'
 
 # read in list of galaxies 
 master_galaxy_list = ascii.read('master_galaxy.list')
 galaxy_list = ascii.read('galaxy.list')
 
 # create ds9 region files for all the galaxies
-all_galaxies_gmc_region_files(master_galaxy_list, data_dir)
+# all_galaxies_gmc_region_files(master_galaxy_list, data_dir)
 
-# print out number of clusters and make the new fits files if needed
-all_galaxies_clust_cats(master_galaxy_list, data_dir, mkfits=False)
+# # print out number of clusters and make the new fits files if needed
+# all_galaxies_clust_cats(master_galaxy_list, data_dir, mkfits=False)
 
-# create ds9 region files for all the galaxies
-all_galaxies_clust_region_files(master_galaxy_list, data_dir, radius_pix=10)
+# # create ds9 region files for all the galaxies
+# all_galaxies_clust_region_files(master_galaxy_list, data_dir, radius_pix=10)
 
-# create ds9 region files for all the HST footprints
-all_galaxies_flc_region_files(galaxy_list, data_dir)
+# # create ds9 region files for all the HST footprints
+# all_galaxies_flc_region_files(galaxy_list, data_dir)
+
+all_galaxies_outline_plots(galaxy_list, data_dir, sc_class='class12')
