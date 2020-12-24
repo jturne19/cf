53a3cf3d56203d6e4ad8c3784580875380dbc230
Jordan Turner <jturne19@uwyo.edu>
2020-12-23 17:38:43 -0700

    runs for classes 123 and the newly added galaxies

--- workflow/catalog_setup.py
+++ workflow/catalog_setup.py
@@ -1,7 +1,7 @@
 """
 main script for compiling the GMC and cluster catalog info and creating all the ncessary ds9 region files
 
-2020-12-16
+2020-12-23
 """
 import numpy as np
 import pandas as pd
@@ -12,6 +12,10 @@ import sys
 sys.path.append('/cherokee1/turner/phangs/cf/utils')
 from utils import *
 
+import matplotlib
+# non-interactive plots
+matplotlib.use('agg')
+
 # set path of the data dir
 data_dir = '/cherokee1/turner/phangs/cf/data/'
 
@@ -20,29 +24,33 @@ master_galaxy_list = ascii.read('master_galaxy.list')
 galaxy_list = ascii.read('galaxy.list')
 
 # create ds9 region files for all the galaxies
-# all_galaxies_gmc_region_files(master_galaxy_list, data_dir)
+# all_galaxies_gmc_region_files(galaxy_list, data_dir)
 
 # # print out number of clusters and make the new fits files if needed
-# all_galaxies_clust_cats(master_galaxy_list, data_dir, mkfits=False)
+# all_galaxies_clust_cats(galaxy_list, data_dir, mkfits=True)
 
 # # create ds9 region files for all the galaxies
-# all_galaxies_clust_region_files(master_galaxy_list, data_dir, radius_pix=10)
+# all_galaxies_clust_region_files(galaxy_list, data_dir, radius_pix=10)
 
 # # create ds9 region files for all the HST footprints
 # all_galaxies_flc_region_files(galaxy_list, data_dir)
 
 
 # play around with radii for plotting the outline plots
-#   	  ngc0628 ngc1433 ngc1559 ngc1566 ngc1792 ngc3351 ngc3627 ngc4535
-radius = [0.04, 0.03, 0.024, 0.03, 0.025, 0.025, 0.04, 0.025]
+#   	  ngc0628 ngc1365 ngc1433 ngc1559 ngc1566 ngc1792 ngc3351 ngc3627 ngc4535 ngc4548 ngc4571
+radius = [0.04, 0.03, 0.03, 0.024, 0.03, 0.025, 0.025, 0.04, 0.025, 0.025, 0.025]
 
 # all_galaxies_outline_plots(galaxy_list, data_dir, sc_class='class12', radius=radius)
+# all_galaxies_outline_plots(galaxy_list, data_dir, sc_class='class123', radius=radius)
 
-# now with the 3-color trilogy images as the background
+# now with the 3-color trilogy images as the background (ngc3627's trilogy image is too big for this)
+radius = [0.04, 0.03, 0.03, 0.024, 0.03, 0.025, 0.025, 0.025, 0.025, 0.025]
 # all_galaxies_outline_plots(galaxy_list, data_dir, sc_class='class12', radius=radius, bkgd='trilogy')
+# all_galaxies_outline_plots(galaxy_list, data_dir, sc_class='class123', radius=radius, bkgd='trilogy')
 
 # outline plots color-coded by cluster age
 # all_galaxies_outline_plots(galaxy_list, data_dir, sc_class='class12', radius=radius, color_code='age')
+# all_galaxies_outline_plots(galaxy_list, data_dir, sc_class='class123', radius=radius, color_code='age')
 
 # generate the mask fits images which define where the HST and ALMA footprints overlap
-generate_overlap_mask(galaxy_list, data_dir)
+# generate_overlap_mask(galaxy_list, data_dir)
